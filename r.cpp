98fe00fbb007cbb9b56bbb33aa8bdc8186a943ca
Azat <azat.malm24@gmail.com>
2021-11-24 21:11:23 +0300

    r.cpp was updated

--- r.cpp
+++ r.cpp
@@ -4,9 +4,9 @@
 #include <climits>
 #include <set>
 
-typedef int64_t dist_t;
-typedef int64_t vertex_t;
-typedef int64_t weight_t;
+typedef int32_t dist_t;
+typedef int32_t vertex_t;
+typedef int32_t weight_t;
 
 struct Point{
     int32_t x;
@@ -18,114 +18,158 @@ struct Point{
 };
 
 class Graph {
-protected:
+public:
     struct GraphNeighboursNode {
         vertex_t vertex_;
         weight_t weight_;
     };
 
-    typedef enum {
-        WHITE_COLOUR = 0,
-        GREY_COLOUR = 1,
-        BLACK_COLOUR = 2,
-    } vertex_colours_t;
+    struct Edge {
+        vertex_t from_;
+        vertex_t to_;
+        weight_t weight_;
+        int32_t number_;
 
+        friend bool operator<(const Edge& first, const Edge& second) {
+            return first.from_ < second.from_ || (first.from_ == second.from_ && first.to_ <= second.to_);
+        }
+
+        friend bool operator==(const Edge& first, const Edge& second) {
+            return first.from_ == second.from_ && first.to_ == second.to_ && first.weight_ == second.weight_;
+        }
+    };
+
+    typedef enum {
+        WHITE_COLOUR,
+        GREY_COLOUR,
+        BLACK_COLOUR,
+    } vertex_colours_t;
+    
+    size_t get_vertexes_num() const {
+        return vertexesNum_;
+    }
+
+    size_t get_edges_num() const {
+        return edgesNum_;
+    }
+
+    static const int32_t UNDEFINED;
+
+    virtual std::vector<vertex_t> get_neighbours_vertex(const vertex_t& vert) const = 0;
+    virtual std::vector<weight_t> get_neighbours_weight(const vertex_t& vert) const = 0;
+    virtual std::vector<GraphNeighboursNode> get_neighbours(const vertex_t& vert) const = 0;
+    virtual void insert_edge(const vertex_t& x, const vertex_t& y, const weight_t& weight = 0) = 0;
+
+protected:
     size_t vertexesNum_;
     size_t edgesNum_;
-    bool isOriented_;
-
-    virtual std::vector<vertex_t> get_neighbours_vertex(const vertex_t& vert) = 0;
-    virtual std::vector<weight_t> get_neighbours_weight(const vertex_t& vert) = 0;
-    virtual std::vector<GraphNeighboursNode> get_neighbours(const vertex_t& vert) = 0;
-
-public:
-    virtual void insert_edge(const vertex_t& x, const vertex_t& y, const weight_t& weight = 0) = 0;
+    bool isOriented_ = false;
 };
 
 class GraphList final : public Graph {
 public:
-    explicit GraphList(size_t vertexesNum, size_t edgesNum) {
+    explicit GraphList(size_t vertexesNum, size_t edgesNum = 0) {
         vertexesNum_ = vertexesNum;
         edgesNum_ = edgesNum;
-        edges_.resize(vertexesNum_, {});
-        used_.resize(vertexesNum_, false);
-        time_up_.resize(vertexesNum_, 0);
-        time_in_.resize(vertexesNum_, 0);
-        time_ = 0;
+        adjacencyList_.resize(vertexesNum_ + 1, {});
     }
 
     void insert_edge(const vertex_t& first, const vertex_t& second, const weight_t& weight = 0) {
-        edges_[first - 1].push_back(second - 1);
-        edges_[second - 1].push_back(first - 1);
-    }
-
-    std::set<vertex_t> find_articulation_points() {
-        std::set<vertex_t> answer;
-
-        for (vertex_t currentVertex = 0; currentVertex < vertexesNum_; ++currentVertex) {
-            if (!used_[currentVertex]) {
-                check_vertex(answer, currentVertex, -1);
-            }
+        if (isOriented_) {
+            insert_edge_oriented(first, second, weight);
+        } else {
+            insert_edge_not_oriented(first, second, weight);
         }
+    }
 
+    std::vector<vertex_t> get_neighbours_vertex(const vertex_t& vert) const {
+        std::vector<vertex_t> answer(adjacencyList_[vert].size());
+        for (size_t i = 0; i < answer.size(); ++i) {
+            answer[i] = adjacencyList_[vert][i].to_;
+        }
         return answer;
     }
 
-protected: // useless in task
-    std::vector<vertex_t> get_neighbours_vertex(const vertex_t& vert) {
-        std::vector<vertex_t> answer = {};
+    std::vector<weight_t> get_neighbours_weight(const vertex_t& vert) const {
+        std::vector<weight_t> answer(adjacencyList_[vert].size());
+        for (size_t i = 0; i < answer.size(); ++i) {
+            answer[i] = adjacencyList_[vert][i].weight_;
+        }
         return answer;
     }
 
-    std::vector<weight_t> get_neighbours_weight(const vertex_t& vert) {
-        std::vector<weight_t> answer = {};
+    std::vector<GraphNeighboursNode> get_neighbours(const vertex_t& vert) const {
+        std::vector<GraphNeighboursNode> answer(adjacencyList_[vert].size());
+        for (size_t i = 0; i < answer.size(); ++i) {
+            answer[i].vertex_ = adjacencyList_[vert][i].to_;
+            answer[i].weight_ = adjacencyList_[vert][i].weight_;
+        }
         return answer;
     }
 
-    std::vector<GraphNeighboursNode> get_neighbours(const vertex_t& vert) {
-        std::vector<GraphNeighboursNode> answer = {};
-        return answer;
-    }
-    
 private:
-    std::vector<std::vector<vertex_t>> edges_; // using adjacency list
-    std::vector<bool> used_;
-    std::vector<int32_t> time_up_;
-    std::vector<int32_t> time_in_;
-    int32_t time_;
-    const int32_t kMaxValue_ = INT_MAX;
+    std::vector<std::vector<Edge>> adjacencyList_; // using adjacency list
 
-    void check_vertex(std::set<vertex_t>& answer, vertex_t currentVertex, vertex_t parent) {
-        int32_t children = 0;
+    void insert_edge_oriented(const vertex_t& first, const vertex_t& second, const weight_t& weight = 0) {
+        adjacencyList_[first].push_back({first, second, weight});
+    }
 
-        used_[currentVertex] = true;
-        time_in_[currentVertex] = time_;
-        time_up_[currentVertex] = time_;
-        ++time_;
-
-        for (auto neighbour : edges_[currentVertex]) {
-            if (neighbour == parent) {
-                continue;
-            }
-            if (used_[neighbour]) {
-                time_up_[currentVertex] = std::min(time_up_[currentVertex], time_in_[neighbour]);
-            } else {
-                check_vertex(answer, neighbour, currentVertex);
-                time_up_[currentVertex] = std::min(time_up_[currentVertex], time_up_[neighbour]);
-
-                if ((time_up_[neighbour] >= time_in_[currentVertex]) && (parent != -1)) {
-                    answer.insert(currentVertex + 1);
-                }
-                
-                children++;
-            }
-        }
-
-        if ((parent == -1) && (children > 1)) {
-            answer.insert(currentVertex + 1);
-        }
+    void insert_edge_not_oriented(const vertex_t& first, const vertex_t& second, const weight_t& weight = 0) {
+        adjacencyList_[first].push_back({first, second, weight});
+        adjacencyList_[second].push_back({second, first, weight});
     }
 };
+
+const int32_t Graph::UNDEFINED = INT_MAX;
+
+void check_vertex_for_bridge(const Graph& graph, std::set<vertex_t>& answer, const vertex_t& currentVertex, const vertex_t& parent, int32_t& time,
+                  std::vector<bool>& used, std::vector<int32_t>& time_in, std::vector<int32_t>& time_up) {
+    int32_t children = 0;
+
+    used[currentVertex] = true;
+    time_in[currentVertex] = time;
+    time_up[currentVertex] = time;
+    ++time;
+
+    for (auto neighbour : graph.get_neighbours_vertex(currentVertex)) {
+        if (neighbour == parent) {
+            continue;
+        }
+        if (used[neighbour]) {
+            time_up[currentVertex] = std::min(time_up[currentVertex], time_in[neighbour]);
+        } else {
+            check_vertex_for_bridge(graph, answer, neighbour, currentVertex, time, used, time_in, time_up);
+            time_up[currentVertex] = std::min(time_up[currentVertex], time_up[neighbour]);
+
+            if ((time_up[neighbour] >= time_in[currentVertex]) && (parent != -1)) {
+                answer.insert(currentVertex);
+            }
+            
+            children++;
+        }
+    }
+
+    if ((parent == -1) && (children > 1)) {
+        answer.insert(currentVertex);
+    }
+}
+
+std::set<vertex_t> find_articulation_points(const Graph& graph) {
+    std::set<vertex_t> answer;
+
+    std::vector<bool> used(graph.get_vertexes_num() + 1, false);
+    std::vector<int32_t> time_up(graph.get_vertexes_num() + 1, 0);
+    std::vector<int32_t> time_in(graph.get_vertexes_num() + 1, 0);
+    int32_t time = 0;
+
+    for (vertex_t currentVertex = 1; currentVertex < graph.get_vertexes_num() + 1; ++currentVertex) {
+        if (!used[currentVertex]) {
+            check_vertex_for_bridge(graph, answer, currentVertex, -1, time, used, time_in, time_up);
+        }
+    }
+
+    return answer;
+}
 
 int main() {
     int32_t vertexNum = 0;
@@ -141,7 +185,7 @@ int main() {
         graph.insert_edge(first, second);
     }
 
-    std::set<vertex_t> answer = graph.find_articulation_points();
+    std::set<vertex_t> answer = find_articulation_points(graph);
 
     std::cout << answer.size() << std::endl;
     for (auto vertex : answer) {
